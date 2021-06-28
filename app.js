5339265448b7cb2805d6248ed0d6a0047daf9f12
Tianjiao Zheng <tzheng@jiaojiao1210gmailcomdeMacBook-Pro.local>
2021-06-27 19:26:41 -0700

    New & Create

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -29,6 +30,16 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 });
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
