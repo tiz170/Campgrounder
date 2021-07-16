aac1ed8b65abdd0592e705652d9106e5b5c081dc
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-07-15 19:09:27 -0700

    update

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: '    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam doloribus qui aliquam ullam veniam, nam non eveniet ipsam saepe inventore, exercitationem id adipisci repellendus ad ipsa consequatur impedit ex excepturi.',
+            price
         })
         console.log(camp);
         await camp.save();
