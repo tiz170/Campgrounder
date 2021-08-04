34f4f166755051daed5c7060dfbba155958e2c2a
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-08-03 22:13:12 -0700

    update

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const reviews = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -31,6 +32,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use('/campgrounds', campgrounds)
