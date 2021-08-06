f2796273c7c42cbdcbd4641901269c63728a271e
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-08-05 23:07:59 -0700

    update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -48,12 +48,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground!');
     res.redirect('/campgrounds');
 }))
 
