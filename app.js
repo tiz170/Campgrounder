cba16fd24fd6f39a017c84a2ce5683c029963997
Tianjiao Zheng <tzheng@jiaojiao1210gmailcomdeMacBook-Pro.local>
2021-08-08 12:49:34 -0700

    update

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const Campground = require('./models/campground');
 const Review = require('./models/review');
@@ -49,12 +52,25 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: 'tizzz@gmail.com', username: 'tizzz' });
+    const newUser = await User.register(user, '170170');
+    res.send(newUser);
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
