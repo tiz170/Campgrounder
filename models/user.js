230d14b7f3607cbe1f8b28adea8632de9b3cdb7d
Tianjiao Zheng <tzheng@jiaojiao1210gmailcomdeMacBook-Pro.local>
2021-08-07 20:29:52 -0700

    update

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = requre('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
