48101e1284d97c8a459edb7a0afe7b960d96c276
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-08-08 23:05:39 -0700

    update

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,8 @@ const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
-})
+});
+
 router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
@@ -26,16 +27,17 @@ router.post('/register', catchAsync(async (req, res, next) => {
 router.get('/login', (req, res) => {
     res.render('users/login');
 })
+
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || 'campgrounds';
+    const redirectUrl = req.session.returnTo || '/campgrounds';
     delete req.session.returnTo;
     res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res) => {
     req.logout();
-    req.flash('success', 'Goodbye!');
+    req.flash('success', "Goodbye!");
     res.redirect('/campgrounds');
 })
 module.exports = router;
