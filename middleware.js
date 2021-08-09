1bb19eef5df9a64d00670d1e2ab1aa2b0d29cb35
Tianjiao Zheng <tzheng@jiaojiao1210gmailcomdeMacBook-Pro.local>
2021-08-08 17:56:42 -0700

    update

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log("REQ.USER...", req.user);
+    // console.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
+        // store the url they are requesting!
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'you must be signed in');
         return res.redirect('/login');
     }
