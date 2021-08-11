8f75e521082c89a7d9db4612032697e472d482c5
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-08-10 19:30:24 -0700

    update

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-
+const { validateReview } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
-const { reviewSchema } = require('../schemas.js');
-
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
