b44ee644a83b367e179ef61c047625b7d54f6421
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-08-12 20:58:32 -0700

    update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,15 +4,16 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.single('image'), (req, res) => {
-        console.log(req.body, req.file);
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
         res.send("IT WORKED!!")
     })
 
