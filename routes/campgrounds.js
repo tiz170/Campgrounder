c189cb370d2efad651e173a4d6fbf878d206ba39
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-08-12 19:51:31 -0700

    update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.single('image'), (req, res) => {
+        console.log(req.body, req.file);
+        res.send("IT WORKED!!")
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
