9e5d9f00c5bd04f18f5f5344a71139944cb67099
Tianjiao Zheng <tzheng@jiaojiao1210gmailcomdeMacBook-Pro.local>
2021-08-21 18:22:22 -0700

    fix thumbnail issue

--- models/campground.js
+++ models/campground.js
@@ -9,6 +9,10 @@ const ImageSchema = new Schema({
     filename: String
 });
 
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
