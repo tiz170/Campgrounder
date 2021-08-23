59d44d5313af5a85487ab74c4a89400b014796da
Tianjiao Zheng <tzheng@jiaojiacomdembp.attlocal.net>
2021-08-23 00:16:05 -0700

    Setting Up Mongo Atlas

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const Review = require('./models/review');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+// const dbUrl = process.env.DB_URL;
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
