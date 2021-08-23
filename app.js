899bfd8da6d551630a2fa6979915e166f82f7b6f
Tianjiao Zheng <tzheng@jiaojiao1210gmailcomdeMacBook-Pro.local>
2021-08-22 17:10:43 -0700

    changes to session

--- app.js
+++ app.js
@@ -54,11 +54,13 @@ app.use(mongoSanitize({
 }));
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshoulebeabettersecret!!!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
